aff7c14874c76ad4d9fe04f7c2a73a67020a9a14
maotaw-cmd <maotawfresh@gmail.com>
2026-07-27 18:38:09 +0200

    Delete MaotawThumbnailDownloader_Fixed_Modular directory

--- MaotawThumbnailDownloader_Fixed_Modular/include/Window.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <windows.h>
-
-// Creates the borderless Win32 window, processes input/messages,
-// initializes Direct2D/DirectWrite/WIC and runs the message loop.
-int RunApplication(HINSTANCE instance, int showCommand);
